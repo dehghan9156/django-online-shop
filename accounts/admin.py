a7928803253dba6f8f384c26f67e8662f14615c5
dehghan9156 <dehghan9156@gmail.com>
2025-07-26 08:57:42 +0330

    Add model User

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import User
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ("email","is_staff","is_active","is_verified")
+    list_filter =  ("email","is_staff","is_active","is_verified")
+    search_fields = ("email",)
+    ordering = ("email",)
+    fieldsets = (
+        ("Authentication", {"fields": ("email", "password")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser", "is_verified")}),
+        ("Group Permissions", {"fields": ("groups", "user_permissions")}),
+        ("Important Date", {"fields": ("last_login",)}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                    "is_superuser",
+                    "is_verified",
+                ),
+            },
+        ),
+    )
+
+admin.site.register(User,CustomUserAdmin)
