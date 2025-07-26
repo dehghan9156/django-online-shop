2a1f1944fccdb31e005a2a44353ca2b936095ed3
dehghan9156 <dehghan9156@gmail.com>
2025-07-26 16:47:15 +0330

    Add register user

--- accounts/views.py
+++ accounts/views.py
@@ -7,8 +7,19 @@ from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.http import HttpResponse
-
+from .forms import *
 
 class RegisterUserView(View):
+    print("test view")
     def get(self,request):
-        pass 
+        form = UserRegisterForm()
+        return render(request,"accounts/user-register.html",{"form":form})
+    
+    def post(self,request):
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = User.objects.create_user(email=cd['email'],password=cd['password'])
+            user.save()
+            return HttpResponse("ok")
+        return render(request,"accounts/user-register.html",{"form":form})
