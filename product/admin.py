73b491f7ce6e8ed77358422d276a16bbb2f96822
dehghan9156 <dehghan9156@gmail.com>
2025-07-27 17:49:09 +0330

    Add models category,product

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
+from django.contrib.admin import ModelAdmin
 
-# Register your models here.
+
+class CustomProduct(ModelAdmin):
+    list_display=("name","price","quantity","discount")
+
+
+
+
+admin.site.register(Category)
+admin.site.register(Product,CustomProduct)
--- product/models.py
+++ product/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Product(models.Model):
+    name = models.CharField(max_length=250)
+    price = models.DecimalField(max_digits=10, decimal_places=2,default=0.0)
+    color = models.CharField(blank=True,null=True)
+    quantity = models.IntegerField()
+    discount = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
