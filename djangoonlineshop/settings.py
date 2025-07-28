15103afa73f7102fec9d7094f30b1853a3ef0e15
dehghan9156 <dehghan9156@gmail.com>
2025-07-28 15:47:56 +0330

    Add app order

--- djangoonlineshop/settings.py
+++ djangoonlineshop/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.humanize',
     'accounts',
     'product',
+    'order',
+
 ]
 
 MIDDLEWARE = [
--- djangoonlineshop/urls.py
+++ djangoonlineshop/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/',include('accounts.urls',namespace='accounts')),
     path('product/',include('product.urls',namespace='product')),
+    path('order/',include('order.urls',namespace='order')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
