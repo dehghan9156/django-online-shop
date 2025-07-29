9e077e7fbfb687792898a8cb64b275817e2f2fcd
dehghan9156 <dehghan9156@gmail.com>
2025-07-29 09:41:01 +0330

    Add model Factor,HeaderFactor

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from .models import * 
 
-# Register your models here.
+
+class CustomHeaderFactor(ModelAdmin):
+    list_display = ("user","status")
+
+class CustomFactor(ModelAdmin):
+    list_display =("product","quantity","total_price","header_factor")
+
+admin.site.register(HeaderFactor,CustomHeaderFactor)
+admin.site.register(Factor,CustomFactor)
