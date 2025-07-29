9d8e25b264922ac0ad4cef5f1e7a258de6b9df97
dehghan9156 <dehghan9156@gmail.com>
2025-07-29 12:10:23 +0330

    Add view for add product basket

--- order/urls.py
+++ order/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
+
 
 app_name="order"
 
 urlpatterns = [
-
+    path("add/product/<int:pk>/",views.AddProductView.as_view(),name="add-product-order"),
 
 ]
--- order/views.py
+++ order/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.contrib.messages import success
+from django.views.generic import ListView, DetailView, UpdateView, DeleteView, CreateView
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.http import HttpResponse
+from django.contrib.auth import authenticate,login,logout
+from django.urls import reverse_lazy,reverse
+from product.models import *
+from .models import * 
 
-# Create your views here.
+
+class AddProductView(LoginRequiredMixin,View):
+    def post(self,request,pk):
+        product = get_object_or_404(Product,pk=pk)
+        header_factor,created = HeaderFactor.objects.get_or_create(user=request.user)
+        factor = Factor.objects.create(product=product,header_factor=header_factor)
+        messages.success(request,"product add successfully","success")
+        return redirect("product:detail-product",pk=pk)
+    
