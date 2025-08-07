c4c2165be333c08bd799867fb769c8e6b347c333
dehghan9156 <dehghan9156@gmail.com>
2025-08-07 08:34:16 +0330

    Add view for ZarinPalPayment

--- order/urls.py
+++ order/urls.py
@@ -3,8 +3,7 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
-
-
+from .views import ZarinPalPaymentView,ZarinPalVerifyView
 app_name="order"
 
 urlpatterns = [
@@ -12,4 +11,6 @@ urlpatterns = [
     path("show/factor/",views.ShowFactorView.as_view(),name="show-factor"),
     path("delete/product/<int:pk>/",views.DeleteProductFactorView.as_view(),name="delete-product"),
     path("update/factor/<int:pk>/",views.UpdateFactorView.as_view(),name="update-factor"),
+    path("payment/<int:pk>/", ZarinPalPaymentView.as_view(), name="payment"),
+    path("payment/verify/<int:pk>/", ZarinPalVerifyView.as_view(),name="payment-verify")
 ]
--- order/views.py
+++ order/views.py
@@ -11,7 +11,7 @@ from django.contrib.auth import authenticate,login,logout
 from django.urls import reverse_lazy,reverse
 from product.models import *
 from .models import * 
-
+import requests
 
 class AddProductView(LoginRequiredMixin,View):
     def post(self,request,pk):
@@ -61,3 +61,79 @@ class UpdateFactorView(View):
         messages.success(request,"quantity product updated","success")
         return redirect("order:show-factor")
     
+
+# مقدار مرچنت کد تستی (Sandbox)
+MERCHANT = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+CALLBACK_URL = "http://127.0.0.1:8001/order/payment/verify/"  # آدرس بازگشت بعد از پرداخت
+
+class ZarinPalPaymentView(View):
+    def get(self, request, pk):
+        """ارسال درخواست پرداخت به زرین‌پال"""
+        try:
+            header_factor = HeaderFactor.objects.get(pk=pk)
+            factors = Factor.objects.filter(header_factor=header_factor)
+            lst = []
+            for factor in factors:
+                lst.append(factor.total_price)
+            total_factor = sum(lst)
+            final_factor = total_factor + 52000
+            amount = int(final_factor)  # مبلغ پرداختی
+            print("ok")
+            data = {
+                "merchant_id": MERCHANT,
+                "amount": amount,
+                "callback_url": f"{CALLBACK_URL}{pk}/",
+                "description": f"پرداخت فاکتور شماره {header_factor.pk}",
+            }
+            headers = {"Content-Type": "application/json"}
+
+            response = requests.post(
+                "https://sandbox.zarinpal.com/pg/v4/payment/request.json",
+                json=data,
+                headers=headers
+            )
+            result = response.json()
+
+            if "data" in result and "authority" in result["data"]:
+                return redirect(f"https://sandbox.zarinpal.com/pg/StartPay/{result['data']['authority']}")
+            else:
+                return render(request, "payment/error.html", {"message": result["errors"]["message"]})
+
+        except HeaderFactor.DoesNotExist:
+            return render(request, "payment/error.html", {"message": "فاکتور یافت نشد."})
+
+class ZarinPalVerifyView(View):
+    def get(self, request,pk):
+        header_factor = HeaderFactor.objects.get(pk=pk)
+        factors = Factor.objects.filter(header_factor=header_factor)
+        lst = []
+        for factor in factors:
+            lst.append(factor.total_price)
+        total_factor = sum(lst)
+        final_price = total_factor + 52000
+        amount = int(final_price)  # مبلغ پرداختی
+
+        """بررسی وضعیت پرداخت بعد از بازگشت از درگاه"""
+        authority = request.GET.get("Authority")
+        data = {
+            "merchant_id": MERCHANT,
+            "amount": amount,
+            "authority": authority
+        }
+        headers = {"Content-Type": "application/json"}
+
+        response = requests.post("https://sandbox.zarinpal.com/pg/v4/payment/verify.json", json=data, headers=headers)
+        result = response.json()
+        print(result)
+        if "data" in result and "code" in result["data"]:
+            if result["data"]["code"] == 100:
+                header_factor.status = "paid"
+                header_factor.save()
+                Factor.objects.filter(header_factor=header_factor).delete()
+                return render(request, "payment/success.html", {"transId": result["data"]["ref_id"]})
+            else:
+                return render(request, "payment/error.html", {"message": f"خطای پرداخت: {result['data']} "})
+        elif "errors" in result:
+            return render(request, "payment/error.html", {"message": f"خطای زرین‌پال: {result['errors']} "})
+        else:
+            return render(request, "payment/error.html", {"message": "پاسخ نامعتبر از زرین‌پال"})
